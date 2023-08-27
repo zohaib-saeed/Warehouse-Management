767b565f3227dc4fc0ef8a7b2c7db552a51d935d
zohaibzyebi <zyebi553@gmail.com>
2023-08-28 04:19:54 +0500

    feat: added image modal to shipment line image item

--- src/components/Home/Subs/ShipmentLines.tsx
+++ src/components/Home/Subs/ShipmentLines.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Tooltip, Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { RiCloseFill as CloseIcon } from 'react-icons/ri';
+
 import Wrapper from '../../Shared/Wrapper';
 import Typography from '../../Shared/Typography';
 import NextImage from '../../Shared/NextImage';
-import { Tooltip } from '@mantine/core';
 import Pagination from '../../Shared/Pagination';
 
 const ShipmentLines: React.FC = () => {
@@ -19,91 +22,117 @@ const ShipmentLines: React.FC = () => {
     'Cancel',
   ];
 
+  // Toggle Modal
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
-    <div className="w-full flex flex-col items-center justify-start ">
-      {/* Heading  */}
-      <Wrapper styles="items-center justify-start py-4 bg-white-main rounded-md">
-        <Typography.PS
-          text="Shipment Lines"
-          additionalStyles="font-bold text-left w-full text-primary"
-        />
-      </Wrapper>
-      {/* Lines Table  */}
-      <div className="w-full flex flex-col items-center justify-start">
-        {/* Header  */}
-        <div className="w-full py-4 px-4 grid grid-cols-[1fr_1fr_2.5fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-3">
-          {tableHeader.map((item, index) => (
-            <Typography.Label
-              key={index}
-              text={item}
-              additionalStyles={`font-medium text-blue-riverbed`}
-            />
-          ))}
-        </div>
-        {/* Body  */}
-        <Wrapper styles="flex-col items-center justify-start bg-white-main rounded-md">
-          {[0, 1, 2, 3, 4].map((item, index) => (
-            <div
-              key={index}
-              className="w-full py-4 border-b-[1px] border-gray-amour grid grid-cols-[1fr_1fr_2.5fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-3 items-center"
-            >
-              {/* Image  */}
-              <NextImage
-                src="/images/sample.jpg"
-                alt=""
-                styles="w-[60px] h-[60px] object-cover rounded overflow-hidden cursor-pointer"
-              />
-              {/* Item Id  */}
+    <React.Fragment>
+      <div className="w-full flex flex-col items-center justify-start ">
+        {/* Heading  */}
+        <Wrapper styles="items-center justify-start py-4 bg-white-main rounded-md">
+          <Typography.PS
+            text="Shipment Lines"
+            additionalStyles="font-bold text-left w-full text-primary"
+          />
+        </Wrapper>
+        {/* Lines Table  */}
+        <div className="w-full flex flex-col items-center justify-start">
+          {/* Header  */}
+          <div className="w-full py-4 px-4 grid grid-cols-[1fr_1fr_2.5fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-3">
+            {tableHeader.map((item, index) => (
               <Typography.Label
-                text="DT3000000_1"
-                additionalStyles="font-medium w-full text-left text-black-mirage"
+                key={index}
+                text={item}
+                additionalStyles={`font-medium text-blue-riverbed`}
               />
-              {/* Desc  */}
-              <Tooltip label="KAPPA Polo Nect T-Shirt" className="bg-black-main text-white-main">
+            ))}
+          </div>
+          {/* Body  */}
+          <Wrapper styles="flex-col items-center justify-start bg-white-main rounded-md">
+            {[0, 1, 2, 3, 4].map((item, index) => (
+              <div
+                key={index}
+                className="w-full py-4 border-b-[1px] border-gray-amour grid grid-cols-[1fr_1fr_2.5fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-3 items-center"
+              >
+                {/* Image  */}
+                <NextImage
+                  onClick={open}
+                  src="/images/sample.jpg"
+                  alt=""
+                  styles="w-[60px] h-[60px] object-cover rounded overflow-hidden cursor-pointer"
+                />
+                {/* Item Id  */}
                 <Typography.Label
-                  text="KAPPA Polo Nect T-Shirt"
+                  text="DT3000000_1"
+                  additionalStyles="font-medium w-full text-left text-black-mirage"
+                />
+                {/* Desc  */}
+                <Tooltip label="KAPPA Polo Nect T-Shirt" className="bg-black-main text-white-main">
+                  <Typography.Label
+                    text="KAPPA Polo Nect T-Shirt"
+                    additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                  />
+                </Tooltip>
+                {/* Color  */}
+                <Typography.Label
+                  text="Black"
                   additionalStyles="font-medium w-full text-left text-black-mirage truncate"
                 />
-              </Tooltip>
-              {/* Color  */}
-              <Typography.Label
-                text="Black"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
-              {/* Size  */}
-              <Typography.Label
-                text="3314"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
-              {/* MRP  */}
-              <Typography.Label
-                text="699"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
-              {/*Current */}
-              <Typography.Label
-                text="4"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
-              {/*Packed  */}
-              <Typography.Label
-                text="2"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
-              {/* Cancel */}
-              <Typography.Label
-                text="0"
-                additionalStyles="font-medium w-full text-left text-black-mirage truncate"
-              />
+                {/* Size  */}
+                <Typography.Label
+                  text="3314"
+                  additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                />
+                {/* MRP  */}
+                <Typography.Label
+                  text="699"
+                  additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                />
+                {/*Current */}
+                <Typography.Label
+                  text="4"
+                  additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                />
+                {/*Packed  */}
+                <Typography.Label
+                  text="2"
+                  additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                />
+                {/* Cancel */}
+                <Typography.Label
+                  text="0"
+                  additionalStyles="font-medium w-full text-left text-black-mirage truncate"
+                />
+              </div>
+            ))}
+            {/* Pagination  */}
+            <div className="w-full flex items-center justify-center py-6">
+              <Pagination />
             </div>
-          ))}
-          {/* Pagination  */}
-          <div className="w-full flex items-center justify-center py-6">
-            <Pagination />
-          </div>
-        </Wrapper>
+          </Wrapper>
+        </div>
       </div>
-    </div>
+      <Modal
+        centered
+        opened={opened}
+        onClose={close}
+        withCloseButton={false}
+        title=""
+        className="w-[370px]"
+      >
+        <div className="w-full flex flex-col items-center justify-start gap-6 pb-4">
+          <CloseIcon
+            onClick={close}
+            className="text-gray-storm text-[24px] ml-auto cursor-pointer"
+          />
+          <NextImage
+            src="/images/sample.jpg"
+            alt=""
+            styles="object-cover w-[350px] h-[350px] rounded-md overflow-hidden"
+          />
+        </div>
+      </Modal>
+    </React.Fragment>
   );
 };
 
--- src/components/Shared/NextImage.tsx
+++ src/components/Shared/NextImage.tsx
@@ -7,11 +7,20 @@ type ImageProps = {
   sizes?: string;
   styles?: string;
   priority?: boolean;
+  onClick?: any;
 };
 
-const NextImage: React.FC<ImageProps> = ({ sizes, src, alt, styles, priority }: ImageProps) => {
+const NextImage: React.FC<ImageProps> = ({
+  sizes,
+  src,
+  alt,
+  styles,
+  priority,
+  onClick,
+}: ImageProps) => {
   return (
     <div
+      onClick={onClick}
       className={`flex items-center justify-center relative ${sizes} ${styles} transition-all duration-300`}
     >
       <Image src={src} fill alt={alt} sizes="sizes" priority={priority} className={`absolute`} />
